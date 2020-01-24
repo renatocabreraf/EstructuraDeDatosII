7522a527af2df2d3f1e4a212a84a3c045ca57942
Renato <renatojose98@yahoo.com>
2020-01-24 11:30:33 -0600

    Lab0 - Controladores
    Creación de controladores y metodos GET y POST

---
+++ Laboratorio0_RenatoCabrera_1010617/Laboratorio0_RenatoCabrera_1010617/Controllers/PeliculasController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Laboratorio0_RenatoCabrera_1010617.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeliculasController : ControllerBase
+    {
+        // GET: api/Peliculas
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Peliculas/5
+        [HttpGet]
+        public IEnumerable<Pelicula> Get()
+        {
+            return "value";
+        }
+
+        // POST: api/Peliculas
+        [HttpPost]
+        public string Test([FromBody]Pelicula peliculas)
+        {
+            var json = JsonConvert.SerializeObject(peliculas);
+            Console.Write("¡Hecho!");
+            return json;
+        }
+
+    }
+}
---
+++ Laboratorio0_RenatoCabrera_1010617/Laboratorio0_RenatoCabrera_1010617/Pelicula.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio0_RenatoCabrera_1010617
+{
+    public class RutaObjeto
+    {
+        public List<Pelicula> Peliculas { get; set; }
+    }
+
+    public class Pelicula
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+
+        public int Anio { get; set; }
+
+        public string Director { get; set; }
+    }
+}
