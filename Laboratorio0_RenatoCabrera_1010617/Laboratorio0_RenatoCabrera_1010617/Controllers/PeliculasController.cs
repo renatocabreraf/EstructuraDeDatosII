8b2e6ceb923352c53b23588266840d9c558b6b89
Renato <renatojose98@yahoo.com>
2020-01-24 11:57:16 -0600

    Lab0
    Solo Falta terminar GET

--- Laboratorio0_RenatoCabrera_1010617/Laboratorio0_RenatoCabrera_1010617/Controllers/PeliculasController.cs
+++ Laboratorio0_RenatoCabrera_1010617/Laboratorio0_RenatoCabrera_1010617/Controllers/PeliculasController.cs
@@ -8,25 +8,17 @@ using Newtonsoft.Json;
 
 namespace Laboratorio0_RenatoCabrera_1010617.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/PeliculasController")]
     [ApiController]
     public class PeliculasController : ControllerBase
     {
-        // GET: api/Peliculas
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET: api/Peliculas/5
         [HttpGet]
         public IEnumerable<Pelicula> Get()
         {
-            return "value";
+            
         }
 
-        // POST: api/Peliculas
         [HttpPost]
         public string Test([FromBody]Pelicula peliculas)
         {
